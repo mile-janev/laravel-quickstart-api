c543df2dbed6154c329402c96794169c050a6c81
Mile Janev <mile.janev89@yahoo.com>
2022-10-12 11:43:27 +0200

    Setup PHP Stan, OpenAPI, PHPCS

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    /**
+     * @OA\Get(
+     *     path="/",
+     *     description="Test page",
+     *     @OA\Response(response="default", description="Test page")
+     * )
+     */
+    public function test()
+    {
+
+    }
+}
