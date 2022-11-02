b5a051f1d951a0527a090dc8a3b57d7d95db9a5c
Mile Janev <mile.janev89@yahoo.com>
2022-11-02 13:55:48 +0100

    Update readme file (#3)

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -5,7 +5,7 @@ return [
 	'documentations' => [
 		'default' => [
 			'api' => [
-				'title' => 'Laravel Starting Package OpenAPI UI',
+				'title' => 'Laravel Quickstart Package OpenAPI UI',
 			],
 
 			'routes' => [
