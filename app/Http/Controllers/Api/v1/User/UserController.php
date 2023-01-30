98e0a5e7ea1ea12a767d101449f3281d3996ba30
Mile Janev <mile.janev89@yahoo.com>
2023-01-30 15:30:13 +0100

    Add strict_types to php files (#9)

--- app/Http/Controllers/Api/v1/User/UserController.php
+++ app/Http/Controllers/Api/v1/User/UserController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Api\v1\User;
 
 use App\Http\Controllers\Controller;
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\v1\Auth\AuthController;
 use App\Http\Controllers\Api\v1\User\UserController;
