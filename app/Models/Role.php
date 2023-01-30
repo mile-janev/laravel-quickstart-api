30b4e47a255d9be810021e8e806dce90a6a76599
Mile Janev <mile.janev89@yahoo.com>
2023-01-30 16:07:43 +0100

    Add soft delete for users and roles

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,14 +5,19 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
+	use SoftDeletes;
+
 	public const ADMIN = "admin";
 	public const USER = "user";
 
 	public const ADMIN_ID = 1;
 	public const USER_ID = 2;
+
+	protected $dates = ['deleted_at'];
 
 	public function users()
 	{
