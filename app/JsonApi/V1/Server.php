a536537654708908af74965e2f438551f3e69ca6
Mile Janev <mile.janev89@yahoo.com>
2023-01-31 03:33:24 +0100

    Fix standards

--- app/JsonApi/V1/Server.php
+++ app/JsonApi/V1/Server.php
@@ -8,33 +8,32 @@ use LaravelJsonApi\Core\Server\Server as BaseServer;
 
 class Server extends BaseServer
 {
+	/**
+	 * The base URI namespace for this server.
+	 *
+	 * @var string
+	 */
+	protected string $baseUri = '/api/v1';
 
-    /**
-     * The base URI namespace for this server.
-     *
-     * @var string
-     */
-    protected string $baseUri = '/api/v1';
+	/**
+	 * Bootstrap the server when it is handling an HTTP request.
+	 *
+	 * @return void
+	 */
+	public function serving(): void
+	{
+		// no-op
+	}
 
-    /**
-     * Bootstrap the server when it is handling an HTTP request.
-     *
-     * @return void
-     */
-    public function serving(): void
-    {
-        // no-op
-    }
-
-    /**
-     * Get the server's list of schemas.
-     *
-     * @return array
-     */
-    protected function allSchemas(): array
-    {
-        return [
-            // @TODO
-        ];
-    }
+	/**
+	 * Get the server's list of schemas.
+	 *
+	 * @return array
+	 */
+	protected function allSchemas(): array
+	{
+		return [
+			// @TODO
+		];
+	}
 }
