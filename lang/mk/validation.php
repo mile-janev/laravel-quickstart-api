f4a9fb8bf15e212bdc1f071011859376394e6aa7
Mile Janev <mile.janev89@yahoo.com>
2023-02-03 11:34:52 +0100

    Add mk localization

--- lang/mk/validation.php
+++ lang/mk/validation.php
@@ -66,7 +66,7 @@ return [
     'image' => 'The :attribute must be an image.',
     'in' => 'The selected :attribute is invalid.',
     'in_array' => 'The :attribute field does not exist in :other.',
-    'integer' => 'The :attribute must be an integer. 22',
+    'integer' => 'The :attribute must be an integer.',
     'ip' => 'The :attribute must be a valid IP address.',
     'ipv4' => 'The :attribute must be a valid IPv4 address.',
     'ipv6' => 'The :attribute must be a valid IPv6 address.',
